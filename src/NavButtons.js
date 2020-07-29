0a1a196331f64d7b6d739c5cca8272418f62b442
Dylan Black <dylan.black2021@gmail.com>
2020-07-28 23:43:23 -0400

    Deleting pokedata and installing MUI

--- src/NavButtons.js
+++ src/NavButtons.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 
 //code from https://github.com/WebDevSimplified/React-Pokemon-Pagination
 
 export default function NavButtons({ id, gotoPrevPage, gotoNextPage }) {
     return (
       <div>
-          {(id - 1) ? <button onClick={gotoPrevPage}>Previous</button> : ""}
-          {(id < 805) && <button onClick={gotoNextPage}>Next</button>}
+          {(id - 1) ? <Button onClick={gotoPrevPage}>Previous</Button> : ""}
+          {(id < 805) && <Button onClick={gotoNextPage}>Next</Button>}
       </div>
     )    
 }
--- src/Pokedata.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Pokedata() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
